7e9e14e33f9309276bcbfc8b5307f39e2a53cdb4
iSharipov <prohodka67@gmail.com>
2014-08-23 01:03:54 +0400

    RIP

---
+++ 15.13v2/run.bat
@@ -0,0 +1,2 @@
+javac -d ./classes -sourcepath ./src ./src/com/getjavajob/sharipov/Main.java
+java -cp ./classes com/getjavajob/sharipov/Main
---
+++ 15.13v2/src/com/getjavajob/sharipov/Main.java
@@ -0,0 +1,11 @@
+package com.getjavajob.sharipov;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		StringStream ss = new StringStream();
+		ss.deleteString();
+
+	}
+
+}
