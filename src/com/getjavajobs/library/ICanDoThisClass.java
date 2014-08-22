2f7893905f74fb61c2f9f832ea48ad861ee74dc1
Roman <romash14021993@gmail.com>
2014-08-22 23:55:17 +0400

    It's my class

---
+++ src/com/getjavajobs/library/ICanDoThisClass.java
@@ -0,0 +1,10 @@
+package com.getjavajobs.library;
+
+/**
+ * Created by Roman on 22.08.14.
+ */
+public class ICanDoThisClass {
+    public static void main(String[] args) {
+        System.out.println("I can do this !!!");
+    }
+}
