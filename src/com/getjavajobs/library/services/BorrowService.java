2ff2ea3d273c97f5a02ee5c18bb148ee6a123333
Roman <romash14021993@gmail.com>
2014-08-24 00:20:22 +0400

    LibraryProject

--- src/com/getjavajobs/library/services/BorrowService.java
+++ src/com/getjavajobs/library/services/BorrowService.java
@@ -5,40 +5,52 @@ import com.getjavajobs.library.exceptions.DAOException;
 import com.getjavajobs.library.exceptions.ServiceException;
 import com.getjavajobs.library.model.Borrow;
 
+import java.util.List;
+
 /**
  * Created by Глеб on 23.08.2014.
  */
 public class BorrowService  {
     public BorrowDao bd = new BorrowDao();
 
-    public Borrow get(int id)throws ServiceException{
+    public Borrow get(int id) throws ServiceException {
         try {
             return bd.get(id);
         }catch (DAOException e){
             throw new ServiceException(e.getMessage(),e);
         }
     }
-    public Borrow add(Borrow borrow)throws ServiceException{
+    public Borrow add(Borrow borrow) throws ServiceException {
         try {
             return bd.add(borrow);
-        }catch (DAOException e){
+        } catch (DAOException e){
             throw new ServiceException(e.getMessage(),e);
         }
     }
-    public void delete(int id)throws ServiceException{
+
+    public void delete(int id) throws ServiceException {
         try {
             bd.delete(id);
-        }catch (DAOException e){
+        } catch (DAOException e){
             throw new ServiceException(e.getMessage(),e);
         }
     }
-    public Borrow update(Borrow borrow)throws ServiceException{
+
+    public Borrow update(Borrow borrow) throws ServiceException {
         try {
             return bd.update(borrow);
-        }catch (DAOException e){
+        } catch (DAOException e){
             throw new ServiceException(e.getMessage(),e);
         }
 
+    }
+
+    public List<Borrow> getAll() throws ServiceException {
+        try {
+            return bd.getAll();
+        } catch (DAOException e) {
+            throw new ServiceException(e.getMessage(),e);
+        }
     }
 
 }
