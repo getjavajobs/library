dcf3297bebaa4e2e56d588e1ab05a6a89d7debca
gegunov <anvares90@mail.ru>
2014-08-24 17:21:13 +0400

    BorrowValidator updated

--- src/com/getjavajobs/library/services/validators/BorrowValidator.java
+++ src/com/getjavajobs/library/services/validators/BorrowValidator.java
@@ -1,7 +1,16 @@
 package com.getjavajobs.library.services.validators;
 
+import com.getjavajobs.library.model.Borrow;
+
 /**
  * Created by Глеб on 23.08.2014.
  */
 public class BorrowValidator {
+    Borrow borrow;
+    public BorrowValidator(Borrow borrow){
+        this.borrow = borrow;
+    }
+    public boolean validate(){
+        return borrow.getDateOfBorrow().before(borrow.getDateOfReturn());
+    }
 }
