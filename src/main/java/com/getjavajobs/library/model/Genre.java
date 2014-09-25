6e09bf701db9b6cd979a73271627e06340229262
Vlad <larionov333@yandex.ru>
2014-09-25 17:30:01 +0700

    GenreDao: добавил equals и hashcode

--- src/main/java/com/getjavajobs/library/model/Genre.java
+++ src/main/java/com/getjavajobs/library/model/Genre.java
@@ -17,5 +17,21 @@ public class Genre {
     public void setGenreType(String inputGenreType){
         genreType=inputGenreType;
     }
-    
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Genre genre = (Genre) o;
+
+        if (genreType != null ? !genreType.equals(genre.genreType) : genre.genreType != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return genreType != null ? genreType.hashCode() : 0;
+    }
 }
