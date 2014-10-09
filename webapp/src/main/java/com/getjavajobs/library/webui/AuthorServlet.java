287bf8fa559f56dfb038d2b09f55091118b93e85
Виталий <belyaevit@gmail.com>
2014-10-09 16:07:34 +0400

    try to read data from entered string ( don't work and needed to  be fixed) Changed  to <url-pattern>/AuthorServlet</url-pattern> fixed action in .jsp

--- webapp/src/main/java/com/getjavajobs/library/webui/AuthorServlet.java
+++ webapp/src/main/java/com/getjavajobs/library/webui/AuthorServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,24 +41,41 @@ public class AuthorServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
+            System.out.println("1");
             String commandType = request.getParameter("commandType");
+            System.out.println("2");
             switch (commandType) {
                 case "add":
                 case "update": {
+                    System.out.println("3");
                     Author author = new Author();
+                    System.out.println("3.1");
                     author.setName(request.getParameter("authorName"));
+                    System.out.println("3.2");
                     author.setSurname(request.getParameter("authorSurname"));
+                    System.out.println("3.3");
                     author.setPatronymic(request.getParameter("authorPatronymic"));
-                    author.setBirthDate(Date.valueOf(request.getParameter("birthDate")));
+                    System.out.println("3.4");
+                    java.util.Date birthDay = null;
+                    try {
+                        birthDay = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("birthDate"));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    author.setBirthDate(birthDay);
+                    System.out.println("3.5");
                     author.setBirthPlace(request.getParameter("birthPlace"));
-
+                    System.out.println("4");
                     if (commandType.equals("add")) {
+                        System.out.println("5");
                         authorService.add(author);
+                        System.out.println("6");
                     } else {
                         int authorId = Integer.valueOf(request.getParameter("authorId"));
                         author.setId(authorId);
                         authorService.update(author);
                     }
+                    System.out.println("7");
                     response.sendRedirect("authors");
                 }
                 break;
