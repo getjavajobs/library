e6a6e07f0329ac41f8b0922c62dab1e93ea92840
Глеб <gegunov@gmail.com>
2014-10-16 16:52:22 +0400

    Переход на контроллеры

--- webapp/src/main/java/com/getjavajobs/library/webui/controllers/BorrowController.java
+++ webapp/src/main/java/com/getjavajobs/library/webui/controllers/BorrowController.java
@@ -12,6 +12,7 @@ import com.getjavajobs.library.services.ReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -37,7 +38,7 @@ public class BorrowController {
     private BorrowService borrowService;
 
     @RequestMapping(value = "/addborrow")
-    public ModelAndView getFreeBooks(){
+    public ModelAndView getFreeBooks() {
         List<Book> bookList = null;
         List<Employee> employeeList = null;
         List<Reader> readerList = null;
@@ -49,39 +50,89 @@ public class BorrowController {
             e.printStackTrace();
         }
         ModelAndView result = new ModelAndView("/WEB-INF/jsp/addBorrow.jsp");
-        result.addObject("freeBooks",bookList);
-        result.addObject("employees",employeeList);
-        result.addObject("readers",readerList);
+        result.addObject("freeBooks", bookList);
+        result.addObject("employees", employeeList);
+        result.addObject("readers", readerList);
         return result;
     }
-    @RequestMapping(value = "/borrows")
-    public ModelAndView getListOfBorrows(){
-        List<Borrow> borrowList = null;
-        try{
-            borrowList = borrowService.getAll();
-        }catch (ServiceException e){
+
+    @RequestMapping(value = "/insertborrow", method = RequestMethod.POST)
+    public String insertBorrow(HttpServletRequest request) {
+        Borrow borrow = new Borrow();
+        int bookId = Integer.parseInt(request.getParameter("book"));
+        int readerId = Integer.parseInt(request.getParameter("reader"));
+        int employeeId = Integer.parseInt(request.getParameter("reader"));
+        Date dateOfReturn = null;
+        Date dateOfBorrow = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        dateOfBorrow = new Date();
+        try {
+            dateOfReturn = sdf.parse(request.getParameter("dateOfReturn"));
+        } catch (ParseException e) {
             e.printStackTrace();
         }
-        ModelAndView result = new ModelAndView("/WEB-INF/jsp/borrow.jsp");
-        result.addObject("borrows",borrowList);
-        return result;
-    }
-    @RequestMapping(value = "/deleteBorrow")
-    public String delBorrow(HttpServletRequest request){
-        int id = Integer.parseInt(request.getParameter("borrowID"));
+        System.out.println(dateOfBorrow);
+        System.out.println(dateOfReturn);
+
         try {
-            bookService.delete(id);
+            borrow.setBook(bookService.get(bookId));
+            borrow.setReader(readerService.get(readerId));
+            borrow.setEmployee(employeeService.get(employeeId));
+            borrow.setDateOfBorrow(dateOfBorrow);
+            borrow.setDateOfReturn(dateOfReturn);
+            borrowService.add(borrow);
         } catch (ServiceException e) {
             e.printStackTrace();
         }
-        return "/WEB-INF/jsp/borrow.jsp";
+        return "redirect:/borrows";
     }
+
+    @RequestMapping(value = "/borrows")
+    public ModelAndView getListOfBorrows() {
+        List<Borrow> borrowList = null;
+        try {
+            borrowList = borrowService.getAll();
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        ModelAndView result = new ModelAndView("/WEB-INF/jsp/borrow.jsp");
+        result.addObject("borrows", borrowList);
+        return result;
+    }
+
+    @RequestMapping(value = "/returnBorrow", method = RequestMethod.GET)
+    public String delBorrow(HttpServletRequest request) {
+        int id = Integer.parseInt(request.getParameter("borrowID"));
+        try {
+            borrowService.delete(id);
+            System.out.println("deleted borrow with id = " + id);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        return "redirect:/borrows";
+    }
+
     @RequestMapping(value = "/updateBorrow")
-    public String updateBorrow(HttpServletRequest request){
+    public ModelAndView redirect(HttpServletRequest request) {
+        ModelAndView result = new ModelAndView("/WEB-INF/jsp/prolong.jsp");
+        Borrow borrow = null;
+        try {
+            borrow = borrowService.get(Integer.parseInt(request.getParameter("borrowID")));
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        result.addObject("dateOfReturn", borrow.getDateOfReturn());
+        result.addObject("borrowId", borrow.getBorrowId());
+        return result;
+    }
+
+    @RequestMapping(value = "/prolong")
+    public String updateBorrow(HttpServletRequest request) {
         Date newDate = null;
         int id = Integer.parseInt(request.getParameter("borrowID"));
         try {
-            newDate = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("dateOfBorrow"));
+            newDate = new SimpleDateFormat("yyyy-MM-dd").parse(request.getParameter("dateOfReturn"));
         } catch (ParseException e) {
             e.printStackTrace();
         }
@@ -92,7 +143,7 @@ public class BorrowController {
         } catch (ServiceException e) {
             e.printStackTrace();
         }
-        return "/WEB-INF/jsp/borrow.jsp";
+        return "redirect:/borrows";
     }
 
 }
